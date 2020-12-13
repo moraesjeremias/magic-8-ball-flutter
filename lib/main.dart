4386463c3dce08397b47b89de720e6d0131ebf5f
Jeremias <moraesjeremias@Jeremiass-MacBook-Pro.local>
2020-12-13 03:24:24 -0300

    init project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:magic_8_ball/screens/home.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
---
+++ lib/screens/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[800],
+        title: Text('Ask Me Anything'),
+      ),
+      body: Container(),
+      backgroundColor: Colors.blue[400],
+    );
+  }
+}
